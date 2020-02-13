a851496832ab7acb26d17355e358323e5fa8d3c5
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-02-13 10:06:55 -0500

    splice

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,20 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
-function spliceElement(someArr, index) {
-  
+function spliceElement(someArr, index) 
+{
+    if (index >= someArr.length)
+    {
+        return 'out of range'
+    }
+    else
+    {
+    let n = someArr.splice (index,1)
+     
+      return someArr
+    }
 }
+console.log(spliceElement (['aaaa','sssss','ddddd','ffffff','gggggg','hhhhhh','bbbbbb','vvvvvv'],7));
+
+/*console.log('This is The Array after removing the element '+`\n` +
+spliceElement (['aaaa','sssss','ddddd','ffffff','gggggg','hhhhhh','bbbbbb','vvvvvv'],1));*/
