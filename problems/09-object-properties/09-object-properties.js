9e16b74dd2efe96882726f88c1e3336965cd7589
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-02-14 09:26:21 -0500

    09

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,13 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
-
+function objectProperties(name,age,location,favColor) 
+{
+    this.name=name;
+    this.age=age;
+    this.location=location;
+    this.favColor=favColor;
 }
+let person = new objectProperties("ahmed",31,"astoria","blue")
+console.log(`The Name is   : ${person.name} \nThe Age       : ${person.age}\nThe Location  : ${person.location}\nThe Color     : ${person.favColor}`);
+ 
